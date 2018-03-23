5a838479d094ecd0691b0f090e5840713c89fc14
Adrian Cho <waihung.cho@hmcts.net>
2018-03-23 09:39:46 +0000

    Add variables to infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,40 +2,9 @@ provider "vault" {
   address = "https://vault.reform.hmcts.net:6200"
 }
 
-data "vault_generic_secret" "s2s_secret" {
-  path = "secret/${var.vault_section}/ccidam/service-auth-provider/api/microservice-keys/cmc"
-}
-
-data "vault_generic_secret" "draft_store_secret" {
-  path = "secret/${var.vault_section}/cmc/draft-store/encryption-secrets/citizen-frontend"
-}
-
-data "vault_generic_secret" "postcode-lookup-api-key" {
-  path = "secret/${var.vault_section}/cmc/postcode-lookup/api-key"
-}
-
-data "vault_generic_secret" "oauth-client-secret" {
-  path = "secret/${var.vault_section}/ccidam/idam-api/oauth2/client-secrets/cmc-citizen"
-}
-
-data "vault_generic_secret" "staff_email" {
-  path = "secret/${var.vault_section}/cmc/claim-store/staff_email"
-}
-
-data "vault_generic_secret" "address_lookup_token" {
-  path = "secret/${var.vault_section}/ccd/postcode-info/token"
-}
-
-data "vault_generic_secret" "oauth2_client_secret" {
-  path = "secret/${var.vault_section}/ccidam/idam-api/oauth2/client-secrets/ccd-gateway"
-}
-
-data "vault_generic_secret" "idam_service_key" {
-  path = "secret/${var.vault_section}/ccidam/service-auth-provider/api/microservice-keys/ccd-gw"
-}
-
 locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  env_ase_url = "${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
 }
 
 module "ccd-case-print-service" {
@@ -46,14 +15,21 @@ module "ccd-case-print-service" {
   ilbIp = "${var.ilbIp}"
   is_frontend  = true
   subscription = "${var.subscription}"
-  additional_host_name = "${var.external_host_name}"
 
   app_settings = {
     // Node specific vars
+    USE_CSRF_PROTECTION = "${var.use_csrf_protection}"
+    SECURITY_REFERRER_POLICY = "${var.security_referrer_policy}"
+    HPKP_MAX_AGE = "${var.hpkp_max_age}"
+    HPKP_SHA256S = "${var.hpkp_sha256s}"
     NODE_ENV = "${var.node_env}"
+    IDAM_PRINT_SERVICE_KEY = ${var.idam_print_service_key}
     UV_THREADPOOL_SIZE = "64"
     NODE_CONFIG_DIR = "D:\\home\\site\\wwwroot\\config"
     TS_BASE_URL = "./src/main"
+
+    CASE_DATA_STORE_URL = "${var.case_data_store_url}"
+    CASE_DATA_PROBATE_TEMPLATE_URL = "${var.case_data_store_url}"
 
     // Logging vars
     REFORM_TEAM = "${var.product}"
@@ -61,39 +37,16 @@ module "ccd-case-print-service" {
     REFORM_ENVIRONMENT = "${var.env}"
 
     // Application vars
-    GA_TRACKING_ID = "${var.ga_tracking_id}"
-    POSTCODE_LOOKUP_API_KEY = "${data.vault_generic_secret.postcode-lookup-api-key.data["value"]}"
-
     // IDAM
     IDAM_API_URL = "${var.idam_api_url}"
     IDAM_AUTHENTICATION_WEB_URL = "${var.authentication_web_url}"
     IDAM_S2S_AUTH = "${var.s2s_url}"
-    IDAM_S2S_TOTP_SECRET = "${data.vault_generic_secret.s2s_secret.data["value"]}"
-    OAUTH_CLIENT_SECRET = "${data.vault_generic_secret.oauth-client-secret.data["value"]}"
 
     IDAM_OAUTH2_TOKEN_ENDPOINT = "${var.idam_api_url}/oauth2/token"
     IDAM_OAUTH2_CLIENT_ID = "ccd_gateway"
-    IDAM_OAUTH2_CLIENT_SECRET = "${data.vault_generic_secret.oauth2_client_secret.data["value"]}"
-    IDAM_LOGOUT_URL = "${var.idam_authentication_web_url}/login/logout"
 
     IDAM_BASE_URL = "${var.idam_api_url}"
     IDAM_S2S_URL = "${var.s2s_url}"
-    IDAM_SERVICE_KEY = "${data.vault_generic_secret.idam_service_key.data["value"]}"
-    IDAM_SERVICE_NAME = "ccd_gw"
-    PROXY_AGGREGATED = "http://ccd-data-store-api-${local.env_ase_url}"
-    PROXY_DATA = "http://ccd-data-store-api-${local.env_ase_url}"
-    PROXY_DEFINITION_IMPORT = "http://ccd-definition-store-api-${local.env_ase_url}"
-    WEBSITE_NODE_DEFAULT_VERSION = "8.9.4"
+    IDAM_SERVICE_NAME = "${var.idam_service_name}"
   }
-}
-
-module "citizen-frontend-vault" {
-  source              = "git@github.com:contino/moj-module-key-vault?ref=master"
-  name                = "cmc-citizen-fe-${var.env}"
-  product             = "${var.product}"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.citizen-frontend.resource_group_name}"
-  product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -32,3 +32,39 @@ variable "subscription" {}
 variable "vault_section" {
   default = "test"
 }
+
+variable "use_csrf_protection" {
+  default = "true"
+}
+
+variable "security_referrer_policy" {
+  default = "origin"
+}
+
+variable "hpkp_max_age" {
+  default = "2592000"
+}
+
+variable "hpkp_sha256s" {
+  default = "[\"replace me\", \"Set proper SHA256s\"]"
+}
+
+variable "idam_print_service_key" {
+  default = "AAAAAAAAAAAAAAAA"
+}
+
+variable "idam_service_name" {
+  default = "ccd_ps"
+}
+
+variable "node_env" {
+  default = "production"
+}
+
+variable "case_data_probate_template_url" {
+  default = "http://localhost:4104"
+}
+
+variable "case_data_store_url" {
+  default = "http://localhost:4452"
+}
