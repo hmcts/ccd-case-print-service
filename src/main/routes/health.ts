f8976761a29b42289c0404594b02bbe15884cd88
Adrian Cho <waihung.cho@hmcts.net>
2018-03-26 16:15:41 +0100

    Resolve build error

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -1,9 +1,15 @@
 import * as healthcheck from "@hmcts/nodejs-healthcheck";
 import * as express from "express";
+import * as os from "os";
 
 const router = express.Router();
 
 router.get("/health", healthcheck.configure({
+  buildInfo: {
+    host: os.hostname(),
+    name: "expressjs-template",
+    uptime: process.uptime(),
+  },
   checks: {
     // empty body for now
   },
