cb8a651542c37945e2feca43fc06c0a27b71818f
Adrian Cho <waihung.cho@hmcts.net>
2018-03-26 15:56:34 +0100

    Remmove checks in health check

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -5,14 +5,8 @@ import * as os from "os";
 const router = express.Router();
 
 router.get("/health", healthcheck.configure({
-  buildInfo: {
-    host: os.hostname(),
-    name: "expressjs-template",
-    uptime: process.uptime(),
-  },
   checks: {
-    // TODO: replace this sample check with proper checks for your application
-    sampleCheck: healthcheck.raw(() => healthcheck.up()),
+    // empty body for now
   },
 }));
 
