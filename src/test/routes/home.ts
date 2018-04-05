eb538f35e80d7bb7b22530d9dc43e58dcbdb2668
Adrian Cho <waihung.cho@hmcts.net>
2018-04-05 18:00:22 +0100

    authorized access

--- src/test/routes/home.ts
+++ src/test/routes/home.ts
@@ -3,13 +3,12 @@ import * as request from "supertest";
 
 import { app } from "../../main/app";
 
-// TODO: replace this sample test with proper route tests for your application
 describe("Home page", () => {
   describe("on GET", () => {
     it("should return sample home page", async () => {
       await request(app)
         .get("/")
-        .expect((res) => expect(res.statusCode).to.equal(200));
+        .expect((res) => expect(res.statusCode).to.equal(401));
     });
   });
 });
