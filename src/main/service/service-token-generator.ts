687a0367445f2c5409b5148c7cbebd65c81d54d1
Lam <daniel.2.lam@atos.net>
2018-06-08 13:00:37 +0100

    Migration to S2S authentication endpoint in CNP
    Changed format of HTTP `POST` request being sent to the `/lease` endpoint, to match S2S authentication in CNP.

--- src/main/service/service-token-generator.ts
+++ src/main/service/service-token-generator.ts
@@ -1,4 +1,3 @@
-import * as FormData from "form-data";
 import * as jwtDecode from "jwt-decode";
 import * as otp from "otp";
 import { fetch } from "../util/fetch";
@@ -19,11 +18,15 @@ export const serviceTokenGenerator = () => {
       return Promise.resolve(cache[serviceName].token);
     } else {
       const oneTimePassword = otp({secret}).totp();
-      const form = new FormData();
-      form.append("microservice", serviceName);
-      form.append("oneTimePassword", oneTimePassword);
+      const form = {
+        microservice: serviceName,
+        oneTimePassword,
+      };
+      const headers = {
+        "Content-Type": "application/json",
+      };
 
-      return fetch(`${idamS2SUrl}/lease`, {method: "POST", body: form})
+      return fetch(`${idamS2SUrl}/lease`, { body: JSON.stringify(form), headers, method: "POST" })
           .then((res) => res.text())
           .then((token) => {
             const tokenData = jwtDecode(token);
