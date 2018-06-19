dd4f934a2db4406840741ea76c73208661e33fcf
Valentin Laurin <me@vlaurin.eu>
2018-06-19 10:41:01 +0100

    Revert "Switch to use CNP S2S authentication instead of Tactical"

--- infrastructure/prod.tfvars
+++ infrastructure/prod.tfvars
@@ -1,5 +1,6 @@
 vault_section = "prod"
 idam_api_url = "https://prod-idamapi.reform.hmcts.net:3511"
+s2s_url = "https://prod-s2s-api.reform.hmcts.net:3511"
 authentication_web_url = "https://www.idam.reform.hmcts.net"
 probate_template_url = "http://betaProdprobateApp01.reform.hmcts.net:4104"
 external_host_name = "return-case-doc.ccd.platform.hmcts.net"
--- src/main/app-insights/app-insights.ts
+++ src/main/app-insights/app-insights.ts
@@ -1,16 +1,12 @@
 const config = require("config");
 const appInsights = require("applicationinsights");
 
-const enabled = config.get("appInsights.enabled");
-
 const enableAppInsights = () => {
-  if (enabled) {
-    const appInsightsKey = config.get("appInsights.instrumentationKey");
-    appInsights.setup(appInsightsKey)
-      .setAutoDependencyCorrelation(true)
-      .setAutoCollectConsole(true, true)
-      .start();
-  }
+  const appInsightsKey = config.get("appInsights.instrumentationKey");
+  appInsights.setup(appInsightsKey)
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectConsole(true, true)
+    .start();
 };
 
 module.exports = enableAppInsights;
