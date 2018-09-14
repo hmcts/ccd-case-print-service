5bdd19b1fd8fb7edf1897ba9d5a96353e3f086f8
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-09-14 15:15:15 +0100

    Infra: default App Service Plan
    Default to use the shared ASP unless overridden in a tfvars file.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,6 +20,8 @@ locals {
   previewResourceGroup = "${var.raw_product}-shared-aat"
   nonPreviewResourceGroup = "${var.raw_product}-shared-${var.env}"
   sharedResourceGroup = "${(var.env == "preview" || var.env == "spreview") ? local.previewResourceGroup : local.nonPreviewResourceGroup}"
+
+  sharedAppServicePlan = "${var.raw_product}-${var.env}"
 }
 
 data "azurerm_key_vault" "ccd_shared_key_vault" {
@@ -42,8 +44,8 @@ module "ccd-case-print-service" {
   is_frontend = "${local.is_frontend}"
   additional_host_name = "${local.external_host_name}"
   common_tags  = "${var.common_tags}"
-  asp_name = "${var.asp_name}"
-  asp_rg = "${var.asp_rg}"
+  asp_name = "${(var.asp_name == "use_shared") ? local.sharedAppServicePlan : var.asp_name}"
+  asp_rg = "${(var.asp_rg == "use_shared") ? local.sharedResourceGroup : var.asp_rg}"
 
   app_settings = {
     // Node specific vars
