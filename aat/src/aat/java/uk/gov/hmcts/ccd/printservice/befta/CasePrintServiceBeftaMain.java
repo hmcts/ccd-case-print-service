2ab9f6ce1405a178e70f8fa75d8fa0fd43187fee
mutlu <mutlu.sancaktutar@hmcts.net>
2020-02-25 19:15:34 +0000

    update as per cucumber runners feature

--- aat/src/aat/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceBeftaMain.java
+++ aat/src/aat/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceBeftaMain.java
@@ -5,8 +5,7 @@ import uk.gov.hmcts.befta.BeftaMain;
 public class CasePrintServiceBeftaMain extends BeftaMain {
 
   public static void main(String[] args) {
-    setTaAdapter(new CasePrintServiceTestAutomationAdapter());
-    BeftaMain.main(args);
+    BeftaMain.main(args, new CasePrintServiceTestAutomationAdapter());
   }
 
 }
