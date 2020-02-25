55137862738bf25cf33ac526c828ca923b60c115
mutlu <mutlu.sancaktutar@hmcts.net>
2020-02-25 20:12:15 +0000

    correct folder structure and cleanup imports

--- aat/src/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceTestAutomationAdapter.java
+++ aat/src/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceTestAutomationAdapter.java
@@ -1,20 +1,8 @@
 package uk.gov.hmcts.ccd.printservice.befta;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import uk.gov.hmcts.befta.BeftaMain;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
-import uk.gov.hmcts.befta.data.UserData;
-import uk.gov.hmcts.befta.exception.FunctionalTestException;
 import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
+
 public class CasePrintServiceTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
     private TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this);
