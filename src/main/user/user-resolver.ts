440495a19b6e3e7abfd013a48002078a41524866
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-03-01 16:56:43 +0000

    RDM-7633 - Migrate Print Service to OIDC

--- src/main/user/user-resolver.ts
+++ src/main/user/user-resolver.ts
@@ -4,7 +4,7 @@ import { fetch } from "../util/fetch";
 export const getTokenDetails = (jwt) => {
   const BEARER_JWT = jwt.startsWith("Bearer ") ? jwt : "Bearer " + jwt;
 
-  return fetch(`${get("idam.base_url")}/details`, {
+  return fetch(`${get("idam.base_url")}/o/userinfo`, {
     headers: {
       Authorization: BEARER_JWT,
     },
