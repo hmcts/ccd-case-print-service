81c0bf6ff5e01249e26b07dda2bb5efd45df2158
mutlu <mutlu.sancaktutar@hmcts.net>
2020-08-05 19:33:34 +0100

    tolerate 50 bytes of difference

--- aat/src/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceTestAutomationAdapter.java
+++ aat/src/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceTestAutomationAdapter.java
@@ -2,6 +2,10 @@ package uk.gov.hmcts.ccd.printservice.befta;
 
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
+import uk.gov.hmcts.befta.util.ReflectionUtils;
+import uk.gov.hmcts.befta.exception.FunctionalTestException;
+import uk.gov.hmcts.befta.exception.FunctionalTestException;
+import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
 
 public class CasePrintServiceTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
@@ -12,4 +16,21 @@ public class CasePrintServiceTestAutomationAdapter extends DefaultTestAutomation
         loader.addCcdRoles();
     }
 
+    @Override
+    public Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
+        if (key.toString().startsWith("approximately ")) {
+            try {
+                String actualSize = (String)ReflectionUtils.deepGetFieldInObject(scenarioContext, "testData.actualResponse.body.__fileInBody__.size");
+                String expectedSize = key.toString().replace("approximately ", "");
+                int actualValue = Integer.parseInt(actualSize);
+                int expectedValue = Integer.parseInt(expectedSize);
+                if(Math.abs(actualValue-expectedValue)<50)
+                    return actualSize;
+                return expectedSize;
+            } catch (Exception e) {
+                throw new FunctionalTestException("Problem checking acceptable response payload size: ", e);
+            }
+        }
+        return super.calculateCustomValue(scenarioContext, key);
+    }
 }
