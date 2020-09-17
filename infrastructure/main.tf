774d1d12926196ba144f6b6e93c4220e7dc36d62
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2020-09-17 13:09:30 +0100

    RDM-9551-TF

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,24 +1,24 @@
 provider "azurerm" {
-  version = "1.22.1"
+  features {}
 }
 
 data "azurerm_key_vault" "ccd_shared_key_vault" {
-  name = "${var.raw_product}-${var.env}"
+  name                = "${var.raw_product}-${var.env}"
   resource_group_name = "${var.raw_product}-shared-${var.env}"
 }
 
 data "azurerm_key_vault" "s2s_vault" {
-  name = "s2s-${var.env}"
+  name                = "s2s-${var.env}"
   resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
 
 data "azurerm_key_vault_secret" "idam_service_key" {
-  name = "microservicekey-ccd-ps"
+  name         = "microservicekey-ccd-ps"
   key_vault_id = "${data.azurerm_key_vault.s2s_vault.id}"
 }
 
 resource azurerm_key_vault_secret "idam_service_secret" {
-  name = "microservicekey-ccd-ps"
-  value = "${data.azurerm_key_vault_secret.idam_service_key.value}"
-  key_vault_id = "${data.azurerm_key_vault.ccd_shared_key_vault.id}"
+  name         = "microservicekey-ccd-ps"
+  value        = data.azurerm_key_vault_secret.idam_service_key.value
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
 }
