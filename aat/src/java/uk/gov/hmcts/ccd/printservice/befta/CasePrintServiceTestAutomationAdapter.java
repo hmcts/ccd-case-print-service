88bda2d9991c07afac8631b679ec2792649c6235
MSancaktutar <mutlu.sancaktutar@hmcts.net>
2020-11-25 19:55:14 +0000

    Format class

--- aat/src/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceTestAutomationAdapter.java
+++ aat/src/java/uk/gov/hmcts/ccd/printservice/befta/CasePrintServiceTestAutomationAdapter.java
@@ -13,32 +13,33 @@ public class CasePrintServiceTestAutomationAdapter extends DefaultTestAutomation
 
     private TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this);
 
-  @Override
-  protected BeftaTestDataLoader buildTestDataLoader() {
-    return new DefaultBeftaTestDataLoader() {
-      @Override
-      public void doLoadTestData() {
-        CasePrintServiceTestAutomationAdapter.this.loader.addCcdRoles();
-        CasePrintServiceTestAutomationAdapter.this.loader.importDefinitions();
-      }
-    };
-  }
+    @Override
+    protected BeftaTestDataLoader buildTestDataLoader() {
+        return new DefaultBeftaTestDataLoader() {
+            @Override
+            public void doLoadTestData() {
+                CasePrintServiceTestAutomationAdapter.this.loader.addCcdRoles();
+                CasePrintServiceTestAutomationAdapter.this.loader.importDefinitions();
+            }
+        };
+    }
 
     @Override
     public Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
-      if (key.toString().startsWith("approximately ")) {
-        try {
-          String actualSize = (String) ReflectionUtils.deepGetFieldInObject(scenarioContext, "testData.actualResponse.body.__fileInBody__.size");
-          String expectedSize = key.toString().replace("approximately ", "");
-          int actualValue = Integer.parseInt(actualSize);
-          int expectedValue = Integer.parseInt(expectedSize);
-          if (Math.abs(actualValue - expectedValue) < 50)
-            return actualSize;
-          return expectedSize;
-        } catch (Exception e) {
-          throw new FunctionalTestException("Problem checking acceptable response payload size: ", e);
+        if (key.toString().startsWith("approximately ")) {
+            try {
+                String actualSize = (String) ReflectionUtils.deepGetFieldInObject(scenarioContext,
+                        "testData.actualResponse.body.__fileInBody__.size");
+                String expectedSize = key.toString().replace("approximately ", "");
+                int actualValue = Integer.parseInt(actualSize);
+                int expectedValue = Integer.parseInt(expectedSize);
+                if (Math.abs(actualValue - expectedValue) < 50)
+                    return actualSize;
+                return expectedSize;
+            } catch (Exception e) {
+                throw new FunctionalTestException("Problem checking acceptable response payload size: ", e);
+            }
         }
-      }
-      return super.calculateCustomValue(scenarioContext, key);
+        return super.calculateCustomValue(scenarioContext, key);
     }
 }
