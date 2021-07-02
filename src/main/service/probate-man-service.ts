b7a74278fb1be20fe822be5c700673047942539c
owen-delaney-cgi <owen.delaney@hmcts.net>
2021-07-02 12:33:32 +0100

    CCD-36 Added logic test for NaN

--- src/main/service/probate-man-service.ts
+++ src/main/service/probate-man-service.ts
@@ -5,13 +5,23 @@ import * as userReqAuth from "../user/user-request-authorizer";
 export function getProbateManLegacyCase(req, probateManType, id) {
   const url = get("case_data_probate_template_url") + "/probateManTypes/" + probateManType + "/cases/" + id;
   const authorization = req.get(userReqAuth.AUTHORIZATION);
-  return fetch(url, {
-                      headers: {
-                        "Authorization": authorization,
-                        "Content-Type": "application/json",
-                        "ServiceAuthorization": req.headers.ServiceAuthorization,
-                      },
-                      method: "GET",
-                    })
-    .then((res) => res.json());
-}
+  if (isNaN(id)){
+    return ERROR_BAD_REQUEST;
+  } else {
+    return fetch(url, {
+                        headers: {
+                          "Authorization": authorization,
+                          "Content-Type": "application/json",
+                          "ServiceAuthorization": req.headers.ServiceAuthorization,
+                        },
+                        method: "GET",
+                      })
+      .then((res) => res.json());
+      }
+    }
+
+export const ERROR_BAD_REQUEST = {
+  error: "Bad Request",
+  message: "Case ID must be a valid luhn number",
+  status: 400,
+};
