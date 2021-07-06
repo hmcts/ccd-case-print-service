f0ee1cadb166544c51860a5dfd68d99d7a8207b5
owen-delaney-cgi <owen.delaney@hmcts.net>
2021-07-06 09:11:24 +0100

    CCD-36 Added logic test for ID being a valid luhn

--- src/main/service/probate-man-service.ts
+++ src/main/service/probate-man-service.ts
@@ -5,13 +5,24 @@ import * as userReqAuth from "../user/user-request-authorizer";
 export function getProbateManLegacyCase(req, probateManType, id) {
   const url = get("case_data_probate_template_url") + "/probateManTypes/" + probateManType + "/cases/" + id;
   const authorization = req.get(userReqAuth.AUTHORIZATION);
-  return fetch(url, {
-                      headers: {
-                        "Authorization": authorization,
-                        "Content-Type": "application/json",
-                        "ServiceAuthorization": req.headers.ServiceAuthorization,
-                      },
-                      method: "GET",
-                    })
+  const luhn = require("luhn");
+  if (luhn.validate(id)) {
+    return fetch(url, {
+      headers: {
+        "Authorization": authorization,
+        "Content-Type": "application/json",
+        "ServiceAuthorization": req.headers.ServiceAuthorization,
+      },
+      method: "GET",
+    })
     .then((res) => res.json());
-}
+  } else {
+      return ERROR_BAD_REQUEST;
+      }
+    }
+
+export const ERROR_BAD_REQUEST = {
+  error: "Bad Request",
+  message: "Case ID must be a valid luhn number",
+  status: 400,
+};
