bb59eee01416d50c715ea968f7a85ac6e762f2c9
Dinesh Patel <dinesh.patel@hmcts.net>
2023-06-07 09:59:27 +0100

    fix for sonar gate failure

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -1,7 +1,12 @@
 import _fetch from "node-fetch";
 
 export const fetch = (...args) => {
-  return _fetch(...args)
+  const schemesList = ["http:", "https:"];
+  const url = (new URL(args[0]));
+  const options = args[1];
+  
+  if (schemesList.includes(url.protocol)) {
+    return _fetch(url,options)
     .then((res) => {
 
       if (res.status >= 200 && res.status < 300) {
@@ -10,4 +15,5 @@ export const fetch = (...args) => {
 
       return Promise.reject(res);
     });
+  }
 };
