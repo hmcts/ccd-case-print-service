3be2d7a0f23096445db3a2adb7d55e76d834b8d2
Dinesh Patel <dinesh.patel@hmcts.net>
2023-06-08 10:25:38 +0100

    removed trailing whitespace

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -4,9 +4,8 @@ export const fetch = (...args) => {
   const schemesList = ["http:", "https:"];
   const url = (new URL(args[0]));
   const options = args[1];
-  
   if (schemesList.includes(url.protocol)) {
-    return _fetch(url,options)
+    return _fetch(url, options)
       .then((res) => {
 
         if (res.status >= 200 && res.status < 300) {
