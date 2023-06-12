26b773e6ec9a9c1eb943ffacc79ec21c47feac11
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2023-06-12 16:17:24 +0100

    Update fetch.ts

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -1,9 +1,9 @@
 import _fetch from "node-fetch";
 
-//comment to trigger sonar scan on this file
+ //comment to trigger sonar scan on this file
 export const fetch = (...args) => {
   return _fetch(...args)
-  //comment to trigger sonar scan on this file
+   //comment to trigger sonar scan on this file
     .then((res) => {
 
       if (res.status >= 200 && res.status < 300) {
