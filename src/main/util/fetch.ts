56cc6cee0b1c2ed0e5bdb809049409ff4bde07dd
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2023-06-13 17:00:01 +0100

    CCD-4712 : fix for sonar gate failure (#478)
    * fix for sonar gate failure
    
    * fix whitespace
    
    * refactored
    
    * removed trailing whitespace
    
    ---------
    
    Co-authored-by: amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -1,13 +1,18 @@
 import _fetch from "node-fetch";
 
 export const fetch = (...args) => {
-  return _fetch(...args)
-    .then((res) => {
+  const schemesList = ["http:", "https:"];
+  const url = (new URL(args[0]));
+  const options = args[1];
+  if (schemesList.includes(url.protocol)) {
+    return _fetch(url, options)
+      .then((res) => {
 
-      if (res.status >= 200 && res.status < 300) {
-          return res;
-      }
+        if (res.status >= 200 && res.status < 300) {
+            return res;
+        }
 
-      return Promise.reject(res);
-    });
+        return Promise.reject(res);
+      });
+  }
 };
