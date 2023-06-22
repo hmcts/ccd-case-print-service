7f276117e1ca46a0ccceacc48b3c163809a41836
Dinesh Patel <dinesh.patel@hmcts.net>
2023-06-22 13:28:13 +0100

    refactored fetch.ts

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -3,8 +3,16 @@ import { get } from "config";
 
 export const fetch = (url, options) => {
   const schemesList = ["http:", "https:"];
-  const domainsList = [get("idam.s2s_url"), get("idam.base_url"), get("case_data_store_url"), get("case_data_probate_template_url")];
+  
+  const url1 = get("idam.s2s_url");
+  const url2 = get("idam.base_url");
+  const url3 = get("case_data_store_url");
+  const url4 = get("case_data_probate_template_url");
+
+  const domainsList = [`${url1}`, `${url2}`, `${url3}`, `${url4}`];
+
   const theUrl = (new URL(url));
+  
   if (schemesList.includes(theUrl.protocol) && domainsList.includes(theUrl.hostname)) {
     return _fetch(theUrl, options)
       .then((res) => {
