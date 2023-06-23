96840c779d645b1101a6649f7a9037d1b84aaef5
Dinesh Patel <dinesh.patel@hmcts.net>
2023-06-23 11:26:10 +0100

    refactored

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -1,40 +1,26 @@
 import _fetch from "node-fetch";
-import { get } from "config";
 
 export const fetch = (url, options) => {
   const schemesList = ["http:", "https:"];
-  const domainListNew = ["localhost"];
+  const domainList = ["localhost"];
 
-  const url1 = get("idam.s2s_url");
-  const url2 = get("idam.base_url");
-  const url3 = get("case_data_store_url");
-  const url4 = get("case_data_probate_template_url");
-
-  const domainsList = [];
-  domainsList.push(url1);
-  domainsList.push(url2);
-  domainsList.push(url3);
-  domainsList.push(url4);
-  domainsList.push("http://localhost:9999");
-
-  console.log("domain list is: " + domainsList);
-  console.log("domain list new is: " + domainListNew);
+  console.log("domain list is: " + domainList);
 
   const theUrl = (new URL(url));
 
   console.log("calling url: " + url);
   console.log("calling url hostname : " + theUrl.hostname);
 
-  if (schemesList.includes(theUrl.protocol) && domainListNew.includes(theUrl.hostname)) {
-    return _fetch(theUrl, options)
-      .then((res) => {
-        if (schemesList.includes(theUrl.protocol) && domainListNew.includes(theUrl.hostname)) {
-          if (res.status >= 200 && res.status < 300) {
-            return res;
-          }
-        }
-
-        return Promise.reject(res);
-      });
+  try {
+    if (schemesList.includes(theUrl.protocol) && domainList.includes(theUrl.hostname)) {
+      const res = _fetch(theUrl, options);
+      if (res.status >= 200 && res.status < 300) {
+        return res;
+      }
+      return Promise.reject(res);
+    }
+  } catch (error) {
+    // TypeError: Failed to fetch
+    console.log('There was an error', error);
   }
 };
