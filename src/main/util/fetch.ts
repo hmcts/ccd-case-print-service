293cc6cc64ad156eba619e6d0b30183e376bb261
Dinesh Patel <dinesh.patel@hmcts.net>
2023-06-23 11:44:49 +0100

    refactored

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -1,26 +1,30 @@
 import _fetch from "node-fetch";
 
 export const fetch = (url, options) => {
-  const schemesList = ["http:", "https:"];
-  const domainList = ["localhost"];
+  (async () => {
+    try {
+      const schemesList = ["http:", "https:"];
+      const domainList = ["localhost"];
 
-  console.log("domain list is: " + domainList);
+      console.log("domain list is: " + domainList);
 
-  const theUrl = (new URL(url));
+      const theUrl = (new URL(url));
 
-  console.log("calling url: " + url);
-  console.log("calling url hostname : " + theUrl.hostname);
+      console.log("calling url: " + url);
+      console.log("calling url hostname : " + theUrl.hostname);
 
-  try {
-    if (schemesList.includes(theUrl.protocol) && domainList.includes(theUrl.hostname)) {
-      const res = _fetch(theUrl, options);
-      if (res.status >= 200 && res.status < 300) {
-        return res;
+      if (schemesList.includes(theUrl.protocol) && domainList.includes(theUrl.hostname)) {
+        const response = await _fetch(theUrl, options)
+        if (response.status >= 200 && response.status < 300) {
+          const json = await response.json()
+          console.log(json.url);
+          console.log(json.explanation);
+          return response;
+        }
+        return Promise.reject(response);
       }
-      return Promise.reject(res);
+    } catch (error) {
+      console.log(error.response.body);
     }
-  } catch (error) {
-    // TypeError: Failed to fetch
-    // console.log(' There was an error ', error);
-  }
+  })();
 };
