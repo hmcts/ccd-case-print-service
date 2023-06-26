1bdca4fad1497763b6907b57bc9868395705f420
Dinesh Patel <dinesh.patel@hmcts.net>
2023-06-26 16:25:24 +0100

    added dummy string

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -2,8 +2,8 @@ import _fetch from "node-fetch";
 
 export const fetch = (...args) => {
   const schemesList = ["http:", "https:"];
-  const url = (new URL(args[0]+""));
-  const options = args[1]+"";
+  const url = (new URL(args[0] + ""));
+  const options = args[1] + "";
   if (schemesList.includes(url.protocol)) {
     return _fetch(url, options)
       .then((res) => {
