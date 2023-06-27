b4dddb3cd650520ac0063f61aac78de80d4f598d
Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>
2023-06-27 11:53:36 +0100

    Update fetch.ts

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -4,7 +4,7 @@ export const fetch = (url, options) => {
   const schemesList = ["http:", "https:"];
   const constUrl = (new URL(url));
   if (schemesList.includes(constUrl.protocol)) {
-    return _fetch(url + "", options)
+    return _fetch(url.replace('\n', '').replace('\r', ''), options)
     .then((res) => {
 
       if (res.status >= 200 && res.status < 300) {
