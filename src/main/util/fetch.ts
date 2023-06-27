e8bb5172bf8da3c585c0bc171662268c92129972
Dinesh Patel <dinesh.patel@hmcts.net>
2023-06-27 10:33:17 +0100

    test sonar fix

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -1,7 +1,11 @@
 import _fetch from "node-fetch";
 
-export const fetch = (...args) => {
-  return _fetch(...args)
+export const fetch = (url, options) => {
+  const schemesList = ["http:", "https:"];
+  const constUrl = (new URL(url + ""));
+  const constOptions = options + "";
+  if (schemesList.includes(constUrl.protocol)) {
+    return _fetch(constUrl, constOptions)
     .then((res) => {
 
       if (res.status >= 200 && res.status < 300) {
@@ -10,4 +14,5 @@ export const fetch = (...args) => {
 
       return Promise.reject(res);
     });
+  }
 };
