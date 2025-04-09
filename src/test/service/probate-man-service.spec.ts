29ba6f321c2f0331a5caa35e1a6fc29e454da558
padmapriyanalam <padmapriya.nalam@gmail.com>
2025-04-09 12:48:28 +0100

    CCD-6123 - dummy PR

--- src/test/service/probate-man-service.spec.ts
+++ src/test/service/probate-man-service.spec.ts
@@ -3,6 +3,7 @@ import * as nock from "nock";
 import * as proxyquire from "proxyquire";
 import * as sinon from "sinon";
 
+// This is a dummy commit.
 describe("probate man service", () => {
 
   let getProbateManLegacyCase;
