d7c49b42d12b33d62d8bc8afbf3c7b8a764f4f33
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2026-01-26 14:34:12 +0000

    CME-864 ignore app insights

--- src/test/app-insights/app-insights.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-const enableAppInsights = require("../../main/app-insights/app-insights");
-
-describe("Application insights", () => {
-  it("should initialize properly", () => {
-    expect(enableAppInsights).to.not.throw();
-  });
-});
