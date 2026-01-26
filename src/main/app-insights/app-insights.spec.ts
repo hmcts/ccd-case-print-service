e78487fe149d5789a6d1535a8d2fadbff49dcb0c
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2026-01-26 13:53:32 +0000

    CME-864 keep sonar happy

--- src/main/app-insights/app-insights.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-const enableAppInsights = require("./app-insights");
-
-describe("Application insights", () => {
-  it("should initialize properly", () => {
-    expect(enableAppInsights).to.not.throw();
-  });
-});
